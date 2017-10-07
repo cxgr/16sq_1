ef013bc6c35cd5233736f95c68991c79cde0e714
cxgr <dmitry.radionov@gmail.com>
2017-10-07 22:35:31 +0300

    more cleanup

--- Assets/Scripts/LevelGen/RoadSection.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class RoadSection : MonoBehaviour
-{
-	public Transform decoRoot;
-	public float decoBorderX;
-}
